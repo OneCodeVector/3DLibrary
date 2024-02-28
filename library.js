f3c6dcb56d0bc5f119adfee30e499e8faf60697f
OneCodeVector <117361107+OneCodeVector@users.noreply.github.com>
2024-02-28 10:19:27 -0500

    Update library.js

--- library.js
+++ library.js
@@ -1,1 +1,35 @@
+const Canvas = document.getElementById("canvas");
+const GL = Canvas.getContext("webgl");
 
+function typeofAccurate(Value) {
+    if (typeof(Value) != "object") {
+        return typeof(Value);
+    }
+
+    // else
+    return Object.getPrototypeOf(Value).constructor.name;
+}
+
+class Vec3 {
+    constructor(X, Y, Z) {
+        this.X = X;
+        this.Y = Y;
+        this.Z = Z;
+    }
+}
+
+class Cube {
+    contructor(Position, Size, Rotation) {
+        this.Position = Position;
+        this.Size = Size;
+        this.Rotation = Rotation;
+    }
+
+    static Draw() {
+        
+    }
+
+    get GetVertecies() {
+
+    }
+}
